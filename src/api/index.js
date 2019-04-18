b0624d65c340ff4b8fcc7ceeaf1a84f9b00bd761
chipueatfast <15520604@gm.uit.edu.vn>
2019-04-18 11:59:32 +0700

    this time it will work

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 export default () => {
     const server = process.env.NODE_ENV === 'production' ? 'http://128.199.134.167' : 'http://localhost:2190';
+    
     return ({
         contract: (contractName) => `${server}/contract/${contractName}`,
         getFactory: (address) => `${server}/factory/${address}`,
