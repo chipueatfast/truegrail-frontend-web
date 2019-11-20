1a7f647d03f2ccf556f3c3a990dd210686e10af1
chipueatfast <15520604@gm.uit.edu.vn>
2019-11-20 22:27:59 +0700

    edit deploykey

--- deploy/ssh_instruction.sh
+++ deploy/ssh_instruction.sh
@@ -3,7 +3,7 @@
 cd /usr/src/truegrail-frontend-web
 echo '------------------------' $(date) '-----------------------' >> ./deployment_log
 eval "$(ssh-agent -s)" >> ./deployment_log 2>&1 # start ssh-agent cache
-ssh-add ~/.ssh/id_rsa_deploy_frontend_web >> ./deployment_log 2>&1
+ssh-add ~/.ssh/truegrail_frontend_deploy_key >> ./deployment_log 2>&1
 git pull --ff >> ./deployment_log 2>&1
 sudo kill $(lsof -t -i:3000) >> ./deployment_log 2>&1
 yarn >> ./deployment_log 2>&1
